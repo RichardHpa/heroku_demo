fe0fd0ca2607b4ef43693d328b896527ae2f6e2e
Richard <richard.m.hpa@gmail.com>
2024-08-23 20:51:47 +1200

    set up basic server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const port = 5001;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
