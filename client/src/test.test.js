9562319956dbe1ae54ebab8aa41e9f8783c3d69e
Richard <richard.m.hpa@gmail.com>
2024-10-02 19:08:38 +1300

    added script for client

--- client/src/test.test.js
+++ client/src/test.test.js
@@ -1,9 +1,8 @@
-// import { checkRunningTournaments } from './checkRunningTournaments';
 import { expect, test } from 'vitest';
 export function sum(a, b) {
   return a + b;
 }
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(4);
+  expect(sum(1, 2)).toBe(3);
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,4 +21,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
